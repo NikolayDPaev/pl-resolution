e26e714edc9516b54cb2ab9dca776ac48ccf3bb4
NikolayDPaev <niki.paev@gmail.com>
2023-07-17 16:43:17 +0300

    Printer added

--- src/Language.elm
+++ src/Language.elm
@@ -1,15 +1,40 @@
-module Formula exposing (..)
+module Language exposing (..)
+
+type alias Language = { 
+    vars : List String,
+    consts : List String,
+    preds : List String,
+    funcs : List String
+    }
 
 type Term
     = Constant String
     | Variable String
-    | Formula String (List Term)
+    | Function String (List Term)
 
+printTerm : Term -> String
+printTerm term =
+    case term of
+        Constant str -> str
+        Variable str -> str
+        Function str terms ->
+            str ++ "(" ++ String.dropRight 2 (List.foldl (\ x acc -> acc ++ (printTerm x) ++ ", ") "" terms) ++ ")"
 
 type Literal
     = PositivePredicate String (List Term)
     | NegativePredicate String (List Term)
 
+printLiteral : Literal -> String
+printLiteral literal = 
+    case literal of
+        PositivePredicate p terms -> 
+            case (List.foldl (\ x acc -> acc ++ (printTerm x) ++ ", ") "" terms) of 
+                "" -> p
+                list -> p ++ "(" ++ String.dropRight 2 list ++ ")"
+        NegativePredicate p terms -> 
+            case (List.foldl (\ x acc -> acc ++ (printTerm x) ++ ", ") "" terms) of 
+                "" -> "¬" ++ p
+                list -> "¬" ++ p ++ "(" ++ String.dropRight 2 list ++ ")"
 
 type Op
     = And
@@ -17,6 +42,13 @@ type Op
     | Impl
     | Eqv
 
+printOp : Op -> String
+printOp op =
+    case op of
+        And -> "&"
+        Or -> "∨"
+        Impl -> "⇒"
+        Eqv -> "⇔"
 
 type Formula
     = Literal Literal
@@ -25,6 +57,14 @@ type Formula
     | Exists String Formula
     | ForAll String Formula
 
+printFormula : Formula -> String
+printFormula f =
+    case f of
+        Literal literal -> printLiteral literal
+        Negation f1 -> "¬" ++ printFormula f1
+        Operation f1 op f2 -> "(" ++ printFormula f1 ++ " " ++ printOp op ++ " " ++ printFormula f2 ++ ")"
+        Exists x f1 -> "∃" ++ x ++ "(" ++ printFormula f1 ++ ")"
+        ForAll x f1 -> "∀" ++ x ++ "(" ++ printFormula f1 ++ ")"
 
 negate : Formula -> Formula
 negate formula =
@@ -39,10 +79,8 @@ negate formula =
         _ ->
             Negation formula
 
-
 type alias Disjunct =
     List Literal
-
 
 eliminateImplAndEqv : Formula -> Formula
 eliminateImplAndEqv formula =
@@ -55,9 +93,11 @@ eliminateImplAndEqv formula =
             case op of
                 Impl -> Operation (negate newA) Or newB
                 Eqv -> Operation (Operation (negate newA) Or newB) And (Operation newA Or (negate newB))
-                _ -> formula
+                _ -> Operation newA op newB
+        Negation f -> Negation (eliminateImplAndEqv f)
+        Exists x f -> Exists x (eliminateImplAndEqv f)
+        ForAll x f -> ForAll x (eliminateImplAndEqv f)
         _ -> formula
-
 
 moveNegations : Formula -> Formula
 moveNegations outerFormula =
@@ -85,8 +125,7 @@ moveNegations outerFormula =
         Exists x f -> Exists x (moveNegations f)
         ForAll x f -> ForAll x (moveNegations f)
 
--- todo: parser
--- todo: printer
+
 
 -- to CNF
 -- to PNF
--- src/Parser.elm
+++ src/Parser.elm
@@ -1,5 +1,12 @@
-module Parser exposing (..)
-import Formula as F exposing (..)
+module Parser exposing (parse)
+import Language exposing (Language)
+import Language as L exposing (..)
+
+type ParseError
+    = ExpectedRightBracket
+    | PredicateInTerm String
+    | UnexpectedToken Token
+    | UnexpectedEnd
 
 type Token
     = Var String
@@ -17,67 +24,132 @@ type Token
     | Not
     | Comma
 
--- tokenizer : String -> List Token
--- tokenizer str = 
---     String.foldr (\el acc ->
---         case el of
---             '(' -> LeftBracket :: acc
---             ')' -> RightBracket :: acc
---             '∀' -> ForAll :: acc
---             '∃' -> Exists :: acc
---             '&' -> And :: acc
---             '∨' -> Or :: acc
---             '↔' -> Eqv :: acc
---             '→' -> Impl:: acc
---             '¬' -> Not :: acc
---             ',' -> Comma :: acc
---             ' ' -> acc
---             c -> case acc of
---                 Name restName :: rest -> Name (String.concat [String.fromChar c, restName]) :: rest
---                 _ -> Name (String.fromChar c) :: acc
+tokenizer : Language -> String -> List Token
+tokenizer lang str = 
+    let basicTokens = String.foldr (\el acc ->
+            case el of
+                '(' -> LeftBracket :: acc
+                ')' -> RightBracket :: acc
+                '∀' -> ForAll :: acc
+                '∃' -> Exists :: acc
+                '&' -> And :: acc
+                '∨' -> Or :: acc
+                '⇔' -> Eqv :: acc
+                '⇒' -> Impl:: acc
+                '¬' -> Not :: acc
+                ',' -> Comma :: acc
+                ' ' -> acc
+                c -> case acc of
+                    Var restName :: rest -> Var (String.concat [String.fromChar c, restName]) :: rest
+                    _ -> Var (String.fromChar c) :: acc
+            )[] str
+    in 
+    List.map (\token -> 
+        case token of
+        Var string ->
+            if List.member string lang.consts then Const string
+            else if List.member string lang.funcs then Func string
+            else if List.member string lang.preds then Pred string
+            else Var string
+        _ -> token
+    ) basicTokens
 
---     )[] str
-
-parseTerm : List Token -> Maybe (Term, List Token)
-parseTerm = Debug.todo "parse Term"
-
-parseTermList : List Token -> Maybe (List Term, List Token)
-parseTermList = Debug.todo "parse term, term, ..., term"
-
-parseHelper : List Token -> Maybe (Formula, List Token)
-parseHelper tokens =
-    case tokens of
-        [] -> Nothing
-        LeftBracket :: rest ->
-            case parseHelper rest of
-                Just (f, restTokens) ->
-                    case restTokens of
-                        RightBracket :: finalRest -> Just (f, finalRest)
-                        _ -> Nothing
-                _ -> Nothing
-        Not :: Pred x :: LeftBracket :: rest ->
+parseTerm : List Token -> Result ParseError (Term, List Token)
+parseTerm tokens =
+    case tokens of 
+        Const string :: rest -> Ok (L.Constant string, rest)
+        Var string :: rest -> Ok (L.Variable string, rest)
+        Func string :: LeftBracket :: rest ->
             case parseTermList rest of
-                Just (terms, restTokens) ->
+                Ok (terms, restTokens) ->
                     case restTokens of
-                        RightBracket :: finalRest -> Just (F.Literal (F.NegativePredicate x terms), finalRest)
-                        _ -> Nothing
-                _ -> Nothing 
-        Pred x :: LeftBracket :: rest ->
-            case parseTermList rest of
-                Just (terms, restTokens) ->
-                    case restTokens of
-                        RightBracket :: finalRest -> Just (F.Literal (F.PositivePredicate x terms), finalRest)
-                        _ -> Nothing
-                _ -> Nothing 
-        ForAll :: Var x :: rest -> 
-            case parseHelper rest of
-                Just (f, restTokens) -> 
-                    Just (F.ForAll x f, restTokens)
-                _ -> Nothing
-        Exists :: Var x :: rest -> 
-            case parseHelper rest of
-                Just (f, restTokens) -> 
-                    Just (F.Exists x f, restTokens)
-                _ -> Nothing
-        -- TODO : operators
-        _ -> Nothing
+                        RightBracket :: finalRest -> Ok (L.Function string terms, finalRest)
+                        _ -> Err ExpectedRightBracket
+                Err e -> Err e
+        Pred p :: _ -> Err (PredicateInTerm p)
+        t :: _ -> Err (UnexpectedToken t)
+        [] -> Err UnexpectedEnd
+                
+
+parseTermList : List Token -> Result ParseError (List Term, List Token)
+parseTermList tokens = 
+    case parseTerm tokens of
+        Ok (firstTerm, rest) ->
+            case rest of
+                Comma :: restTokens ->
+                    case parseTermList restTokens of
+                        Ok (termList, finalRest) -> Ok (firstTerm :: termList, finalRest)
+                        Err e -> Err e
+                _ -> Ok ([firstTerm], rest)
+        Err e -> Err e
+
+
+parseFormula : List Token -> Result ParseError (Formula, List Token)
+parseFormula tokens =
+    let resultFirstFormula =
+            case tokens of
+                [] -> Err UnexpectedEnd
+                LeftBracket :: rest ->
+                    case parseFormula rest of
+                        Ok (f, restTokens) ->
+                            case restTokens of
+                                RightBracket :: finalRest -> Ok (f, finalRest)
+                                _ -> Err ExpectedRightBracket
+                        Err e -> Err e
+                Not :: rest ->
+                    case parseFormula rest of
+                        -- Ok (F.Literal (F.PositivePredicate x terms), restTokens) ->
+                        --     Ok (F.Literal (F.NegativePredicate x terms), restTokens)
+                        -- Ok (F.Literal (F.NegativePredicate x terms), restTokens) ->
+                        --     Ok (F.Literal (F.PositivePredicate x terms), restTokens)
+                        Ok ((L.Operation f1 op f2), restTokens) -> Ok ((L.Operation (L.Negation f1) op f2), restTokens)
+                        Ok (formula, restTokens) -> Ok (L.Negation formula, restTokens)
+                        Err e -> Err e 
+                Pred x :: LeftBracket :: RightBracket :: rest ->
+                    Ok (L.Literal (L.PositivePredicate x []), rest)
+                Pred x :: LeftBracket :: rest ->
+                    case parseTermList rest of
+                        Ok (terms, restTokens) ->
+                            case restTokens of
+                                RightBracket :: finalRest -> Ok (L.Literal (L.PositivePredicate x terms), finalRest)
+                                _ -> Err ExpectedRightBracket
+                        Err e -> Err e 
+                ForAll :: Var x :: rest -> 
+                    case parseFormula rest of
+                        Ok (f, restTokens) -> 
+                            Ok (L.ForAll x f, restTokens)
+                        Err e -> Err e
+                Exists :: Var x :: rest -> 
+                    case parseFormula rest of
+                        Ok (f, restTokens) -> 
+                            Ok (L.Exists x f, restTokens)
+                        Err e -> Err e
+                t :: _ -> Err (UnexpectedToken t)
+    in
+    Result.andThen (\(firstFormula, rest) ->
+        case rest of
+            And :: restTokens ->
+                case parseFormula restTokens of
+                    Ok (secondFormula, finalTokens) -> Ok ((L.Operation firstFormula L.And secondFormula),  finalTokens)
+                    Err e -> Err e
+            Or :: restTokens ->
+                case parseFormula restTokens of
+                    Ok (secondFormula, finalTokens) -> Ok ((L.Operation firstFormula L.Or secondFormula),  finalTokens)
+                    Err e -> Err e
+            Impl :: restTokens ->
+                case parseFormula restTokens of
+                    Ok (secondFormula, finalTokens) -> Ok ((L.Operation firstFormula L.Impl secondFormula),  finalTokens)
+                    Err e -> Err e
+            Eqv :: restTokens ->
+                case parseFormula restTokens of
+                    Ok (secondFormula, finalTokens) -> Ok ((L.Operation firstFormula L.Eqv secondFormula),  finalTokens)
+                    Err e -> Err e
+            _ -> Ok (firstFormula, rest)
+    ) resultFirstFormula
+        
+parse : Language -> String -> Result ParseError Formula
+parse lang string =
+    case parseFormula (tokenizer lang string) of
+        Ok (formula, []) -> Ok formula
+        Ok (_, x :: _) -> Err (UnexpectedToken x)
+        Err e -> Err e
