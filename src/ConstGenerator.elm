5747e4d7060fa21b24f4c4df7b76d240e690d793
NikolayDPaev <niki.paev@gmail.com>
2023-07-18 12:04:58 +0300

    Generator for new constants implemented

--- src/ConstGenerator.elm
+++ src/ConstGenerator.elm
@@ -1,22 +1,27 @@
 module ConstGenerator exposing (..)
 
-type alias Generator = List String
-
-createGenerator : Generator
-createGenerator = ["a", "b", "c", "d"]
+type alias Generator = {
+        state: List String,
+        forbidden: List String
+    }
+createGenerator : List String -> Generator
+createGenerator forbidden = {forbidden = forbidden, state = ["a", "b", "c", "d"]}
 
 getConst : Generator -> (String, Generator)
 getConst generator =
     let 
-        const = Maybe.withDefault "e" (List.head generator)
-        newGenerator = Maybe.withDefault [] (List.tail generator)
+        const = Maybe.withDefault "e" (List.head generator.state)
+        newState = Maybe.withDefault [] (List.tail generator.state)
         newConst = 
             let lastLetter = Maybe.withDefault '0' <| Maybe.map Tuple.first <| String.uncons <| String.right 1 const
             in
                 if (Char.isAlpha lastLetter) || (lastLetter == '9') then const ++ "0"
                 else (String.dropRight 1 const) ++ String.fromChar (Char.fromCode (Char.toCode lastLetter + 1))
+        newGenerator = {generator | state = List.append newState [newConst]}
     in
-    (const, List.append newGenerator [newConst])
+    if List.member const generator.forbidden then 
+        getConst newGenerator
+    else (const, newGenerator )
 
 getNConsts : Int -> Generator -> (List String, Generator)
 getNConsts n generator =
